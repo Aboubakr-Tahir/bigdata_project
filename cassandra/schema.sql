e06c69d9b7433a8f71ef134bff03c86806dd8131
Aboubakr-Tahir <aboubakrtahir44@gmail.com>
2026-03-26 21:13:58 +0100

    adding : cassandra schema , telematry script , nifi template

---
+++ cassandra/schema.sql
@@ -0,0 +1,16 @@
+-- Création du Keyspace (L'équivalent d'une Database)
+CREATE KEYSPACE IF NOT EXISTS smart_city 
+WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};
+
+-- Utilisation du Keyspace
+USE smart_city;
+
+-- Création de la table pour tes métriques
+CREATE TABLE IF NOT EXISTS telemetry (
+    device_id text,
+    timestamp timestamp,
+    cpu_usage_percent float,
+    ram_available_mb float,
+    temperature_c float,
+    PRIMARY KEY (device_id, timestamp)
+) WITH CLUSTERING ORDER BY (timestamp DESC);
---
+++ edge/edge_agent.py
@@ -0,0 +1,56 @@
+import psutil
+import time
+import requests
+from datetime import datetime
+
+# --- CONFIGURATION ---
+URL = "http://100.82.235.96:9999/content"
+DEVICE_ID = "debian-edge-01"
+SEND_INTERVAL = 0.5  # Envoi toutes les 0.5 secondes (très fluide)
+
+def get_metrics():
+    """Récupère les métriques système en temps réel."""
+    cpu_usage = psutil.cpu_percent(interval=None)
+
+    ram = psutil.virtual_memory()
+    ram_available_mb = round(ram.available / (1024 * 1024), 2)
+
+    temp = None
+    sensors = psutil.sensors_temperatures()
+    if 'coretemp' in sensors:
+        temp = sensors['coretemp'][0].current
+    elif 'cpu_thermal' in sensors:
+        temp = sensors['cpu_thermal'][0].current
+
+    return {
+        "timestamp": datetime.utcnow().isoformat() + "Z",
+        "device_id": DEVICE_ID,
+        "cpu_usage_percent": cpu_usage,
+        "ram_available_mb": ram_available_mb,
+        "temperature_c": temp
+    }
+
+def main():
+    print(f"🚀 Démarrage de l'Agent Edge Python sur {DEVICE_ID}...")
+    print(f"📡 Cible : {URL}")
+
+    try:
+        while True:
+            # 1. Génération des données
+            payload = get_metrics()
+
+            # 2. Envoi direct en mémoire (bypasse le disque dur lent)
+            try:
+                response = requests.post(URL, json=payload, timeout=2)
+                print(f"[+] Envoyé à {payload['timestamp']} | Status: {response.status_code} | CPU: {payload['cpu_usage_percent']}%")
+            except requests.exceptions.RequestException as e:
+                print(f"[-] Erreur réseau (le PC principal est-il allumé ?) : {e}")
+
+            # 3. Attente avant le prochain envoi
+            time.sleep(SEND_INTERVAL)
+
+    except KeyboardInterrupt:
+        print("\n🛑 Arrêt de l'agent par l'utilisateur.")
+
+if __name__ == "__main__":
+    main()
